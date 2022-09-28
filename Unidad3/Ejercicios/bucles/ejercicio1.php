c5e7c964b5d6bee35991fe80ba209009083364df
Raul Pantoja <60599710+RaulPantoja@users.noreply.github.com>
2022-09-28 13:37:33 +0200

    Add files via upload

--- Unidad3/Ejercicios/bucles/ejercicio1.php
+++ Unidad3/Ejercicios/bucles/ejercicio1.php
@@ -1,1 +1,12 @@
+<?php
+/**
+ * Ejercicio 1 de bucles
+ * Escribir los números 1 al 10
+ * @author Raul Pantoja
+ */
 
+
+for ($a = 1; $a <= 10; $a++) {
+    echo  $a . ",";
+}
+?>
---
+++ Unidad3/Ejercicios/bucles/ejercicio2.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Ejercicio 2 de php
+ * Sumar los 3 primeros números pares.
+ * @author Raul Pantoja
+ */
+
+
+$total = 0;
+ for ($i = 0; $i <= 3 ; $i++) {
+    $total += $i*2;
+ };
+ echo $total;
+
+ echo '<a href="https://github.com/RaulPantoja/DesarrolloWebServidor/blob/main/Unidad3/Ejercicios/ejercicio2.php">GitHub</a>';
+ ?>
+
